1a0b9222c22dc7ac41dc23938c6bbea6979f4bfa
xxjcaxx <jocasal@gmail.com>
2021-09-15 10:35:55 +0200

    dni ex 1 dom

--- dni/scripts.js
+++ dni/scripts.js
@@ -1,28 +1,34 @@
-"use strict";
-//let lletres = "TRWAGMYFPDXBNJZSQVHLCKET".split('');
+(function () {
+  "use strict";
+  let lletres = "TRWAGMYFPDXBNJZSQVHLCKET".split("");
 
-function validar(dni) {
+  function validar(dni) {
     if (!isNaN(dni) && dni.length == 8) {
-        return true;
+      return true;
+    } else {
+      return false;
     }
-    else {
-        return false;
-    }
-}
+  }
 
-function lletra(dni) {
-    let lletres = ["T", "R", "W", "A", "G", "M", "Y", "F", "P",
+  function lletra(dni) {
+    /*let lletres = ["T", "R", "W", "A", "G", "M", "Y", "F", "P",
         "D", "X", "B", "N", "J", "Z", "S",
-        "Q", "V", "H", "L", "C", "K", "E", "T"];
+        "Q", "V", "H", "L", "C", "K", "E", "T"];*/
     return lletres[parseInt(dni) % 23];
-}
+  }
 
-function calcularLletra(element) {
+  function calcularLletra() {
     //console.log(document.querySelector("#dni").value);
-    let dni = element.value;
+    let dni = this.value;
     console.log(dni);
     if (validar(dni)) {
-        console.log(lletra(dni));
+      console.log(lletra(dni));
+    } else {
+      console.log("El DNI no és vàlid");
     }
-    else { console.log("El DNI no és vàlid"); }
-}
+  }
+
+  document.addEventListener("DOMContentLoaded", function () {
+    document.querySelector("#dni").addEventListener("click", calcularLletra);
+  });
+})();
