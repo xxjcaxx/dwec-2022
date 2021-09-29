0bad40420b9f6ea018c7fc863d79cc0b49559ba2
xxjcaxx <jocasal@gmail.com>
2021-09-29 09:44:01 +0200

    exemple this

--- dni/scripts.js
+++ dni/scripts.js
@@ -3,18 +3,41 @@
   const lletres = "TRWAGMYFPDXBNJZSQVHLCKET".split("");
   const arrayDNIs = ["124312", "12345678", "sadf21123", "11111111", "22222222"];
 
-  function validar(dni) {
-    if (!isNaN(dni) && dni.length == 8) {
-      return true;
-    } else {
-      return false;
+  class DNI {
+    constructor(dni) {
+      this.dni = "";
+      this.lletra = "";
+      if (DNI.validar(dni)) {
+        this.dni = dni;
+        this.lletra = DNI.calcularLletra(dni);
+      }
+    }
+    static validar(dni) {
+      if (!isNaN(dni) && dni.length == 8) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+    static calcularLletra(dni) {
+      return lletres[parseInt(dni) % 23];
+    }
+    dniComplet() {
+      return `${this.dni}${this.lletra}`;
     }
   }
 
+  let arrayObjectesDNI = [];
+
+  for (let d of arrayDNIs) {
+    arrayObjectesDNI.push(new DNI(d));
+  }
+
+  console.log(arrayObjectesDNI);
+
+  /*
   function lletra(dni) {
-    /*let lletres = ["T", "R", "W", "A", "G", "M", "Y", "F", "P",
-        "D", "X", "B", "N", "J", "Z", "S",
-        "Q", "V", "H", "L", "C", "K", "E", "T"];*/
+   
     return lletres[parseInt(dni) % 23];
   }
 
@@ -29,6 +52,18 @@
     }
   }
 
+  function calcularDNIs() {
+    let lletraUsuari = document.querySelector("#lletra").value;
+    let posicio = lletres.indexOf(lletraUsuari);
+    console.log("Calculant DNI de la lletra: ", lletraUsuari, posicio);
+    let llistaDNIs = [];
+    for (let i = 0; i < 20; i++) {
+      let dni = Math.floor(Math.random() * 4347825) * 23 + posicio;
+      llistaDNIs.push(dni + lletra(dni));
+    }
+    console.log(llistaDNIs);
+  }
+
   for (let d of arrayDNIs) {
     if (validar(d)) {
       console.log(`DNI: ${d}${lletra(d)}`);
@@ -37,15 +72,27 @@
     }
   }
 
-  for (let i = 0; i < arrayDNIs.length; i++) {
-    if (validar(arrayDNIs[i])) {
-      console.log(`DNI: ${d}${lletra(arrayDNIs[i])}`);
-    } else {
-      console.log("El DNI no és vàlid");
-    }
-  }
-
   document.addEventListener("DOMContentLoaded", function () {
     document.querySelector("#dni").addEventListener("click", calcularLletra);
-  });
+    document
+      .querySelector("#calcularDNIs")
+      .addEventListener("click", calcularDNIs);
+  });*/
 })();
+
+function UiComponent() {
+  this.name = "UiComponent";
+  //var _this = this;
+  var button = document.querySelector("#header");
+  button.UiComponent = this;
+  button.addEventListener("click", function () {
+    console.log("CLICK", this);
+    this.UiComponent.handleClick();
+    //this.handleClick();
+  });
+}
+UiComponent.prototype.handleClick = function () {
+  console.log(this);
+};
+
+//let ui = new UiComponent();
