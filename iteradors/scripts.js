4145050ae41cfb39766c76771fd3ce2bcd8b5f52
xxjcaxx <jocasal@gmail.com>
2021-10-15 09:44:25 +0200

    divendres 15

--- iteradors/scripts.js
+++ iteradors/scripts.js
@@ -16,15 +16,67 @@ import { pokemons } from "./iteradors.js";
     let mitjana =
       total.reduce((anterior, actual) => anterior + actual.base.Attack, 0) /
       total.length;
-    console.log(total, mitjana);
+    // console.log(total, mitjana);
   });
 })();
 
-let phrase = "Hola";
-if (true) {
-  let user = "Juan";
-  function sayHi() {
-    alert(`${phrase}, ${user}`);
+function Persona(nom, edad) {
+  this.nom = nom;
+  this.edad = edad;
+}
+Persona.prototype.showEdad = function () {
+  console.log(this.edad);
+};
+
+class PERSONA {
+  constructor(nom, edad) {
+    this.nom = nom;
+    this.edad = edad;
+  }
+  showEdad() {
+    console.log(this.edad);
   }
 }
-sayHi();
+
+let array = [
+  new Persona("Jose", 25),
+  new PERSONA("Juan", 24),
+  {
+    nom: "Juan",
+    edad: 24,
+    showEdad: function () {
+      console.log(this.edad);
+    },
+  },
+  { nom: "Nuria", edad: -5 },
+  { nom: "Javi", edad: 35 },
+  { nom: "Andreu", edad: 15 },
+  { nom: "Pepe", edad: -1 },
+];
+
+let menor = array.filter(function (persona) {
+  return persona.edad < 0;
+});
+
+/*menor = [];
+for (let persona of array) {
+  if (persona.edad < 0) {
+    menor.push(persona);
+  }
+}*/
+
+console.log(menor);
+
+let numeros = array.map((persona) => persona.edad);
+/*
+numeros = [];
+for(let persona of array){
+  numeros.push(persona.edad);
+}
+*/
+console.log(numeros);
+
+let mitjana =
+  array.reduce((anterior, actual) => anterior + actual.edad, 0) / array.length;
+
+console.log(mitjana);
