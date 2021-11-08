eabe5c926d3e1421797ab7e7526b24e26b0b1009
xxjcaxx <jocasal@gmail.com>
2021-11-08 10:45:38 +0100

    dilluns 8

--- comunicacio_servidor/apiMarvel/scripts.js
+++ comunicacio_servidor/apiMarvel/scripts.js
@@ -1,7 +1,109 @@
 (() => {
-  async function getCharacters() {
+  let charactersList = [];
+  let pagines = 0;
+  let off = 0;
+  let lim = 20;
+
+  async function allCharacters(container, offset, limit) {
+    container.innerHTML = "";
+    // if (charactersList.length == 0) {
+    charactersList = await getCharacters(offset, limit);
+    //}
+    charactersList.map((c) => {
+      c.renderCard(container);
+    });
+
+    // Paginacio
+    let divPagines = document.createElement("div");
+    //divPagines.classList.add("btn-group");
+    for (let i = 0; i < pagines; i++) {
+      let paginaButton = document.createElement("button");
+      paginaButton.classList.add("btn");
+
+      if (i * 20 == off) {
+        paginaButton.classList.add("btn-primary");
+      } else {
+        paginaButton.classList.add("btn-secondary");
+      }
+
+      paginaButton.innerHTML = i;
+      paginaButton.addEventListener("click", () => {
+        //console.log("Pagina", i);
+        off = i * 20;
+        allCharacters(container, off, lim);
+      });
+
+      divPagines.append(paginaButton);
+    }
+    document.querySelector("#pagines").innerHTML = "";
+    document.querySelector("#pagines").append(divPagines);
+  }
+
+  class Character {
+    constructor(dades) {
+      Object.assign(this, dades);
+      this.comicsDetails = [];
+      this.thumbnail = `${this.thumbnail.path}/standard_fantastic.${this.thumbnail.extension}`;
+    }
+    renderCard(container) {
+      let divCharacter = document.createElement("div");
+      divCharacter.innerHTML = `
+      <div class="col">
+      <div class="card" style="width: 15rem;">
+      <img src="${this.thumbnail}" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">${this.name}</h5>
+        <p class="card-text">${this.description}</p>
+        <a href="#" class="btn btn-primary">Details</a>
+      </div>
+    </div>
+    </div>
+        `;
+      container.append(divCharacter);
+      divCharacter.querySelector(".btn").addEventListener("click", async () => {
+        let characterDetail = await getCharacterDetail(this.id);
+        // console.log(characterDetail);
+        characterDetail.renderDetails(container);
+      });
+    }
+    renderDetails(container) {
+      let divCharacter = document.createElement("div");
+      divCharacter.innerHTML = `
+      <div class="col">
+      <div class="card" style="width: 40rem;">
+      <img src="${this.thumbnail}" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">${this.name}</h5>
+        <p class="card-text">${this.description}</p>
+        <ul class="comicsList list-group">
+        </ul>
+        <a href="#" class="btn btn-primary">Return</a>
+      </div>
+    </div>
+    </div>
+        `;
+      container.innerHTML = "";
+      container.append(divCharacter);
+      let ulComics = divCharacter.querySelector(".comicsList");
+      this.comicsDetails.map((c) => {
+        let liComic = document.createElement("li");
+        liComic.classList.add("list-group-item");
+        liComic.innerHTML = `
+        <h6>${c.title}</h6>
+        <p>${c.description}</p>
+        `;
+        ulComics.append(liComic);
+      });
+      //divCharacter.append(ulComics);
+      divCharacter.querySelector(".btn").addEventListener("click", () => {
+        allCharacters(container, off, lim);
+      });
+    }
+  }
+
+  async function getCharacters(offset, limit) {
     let response = await fetch(
-      "https://gateway.marvel.com:443/v1/public/characters?apikey=09186f978ec0616e9dba9c4ac4b0c4bb"
+      `https://gateway.marvel.com:443/v1/public/characters?limit=${limit}&offset=${offset}&apikey=09186f978ec0616e9dba9c4ac4b0c4bb`
     );
     let data = await response.json();
 
@@ -13,12 +115,52 @@
           console.log(data);
       });*/
 
-    return data.data.results;
+    // PAGINACIO
+
+    let total = data.data.total;
+    pagines = total / 20;
+
+    return data.data.results.map((c) => new Character(c));
   }
 
-  document.addEventListener("DOMContentLoaded", () => {
-    getCharacters().then((characters) => {
-      console.log(characters);
+  async function getCharacterDetail(id) {
+    let response = await fetch(
+      `https://gateway.marvel.com:443/v1/public/characters/${id}?apikey=09186f978ec0616e9dba9c4ac4b0c4bb`
+    );
+    let data = await response.json();
+    console.log(data);
+    let character = new Character(data.data.results[0]);
+    let comics = character.comics.items.map((c) =>
+      fetch(`${c.resourceURI}?apikey=09186f978ec0616e9dba9c4ac4b0c4bb`)
+    );
+
+    let comicResponses = await Promise.all(comics);
+    let comicList = await Promise.all(comicResponses.map((c) => c.json()));
+
+    comicList.map((c) => {
+      character.comicsDetails.push(c.data.results[0]);
     });
+
+    /* Promise.all(comics)
+      .then((comicResponses) => {
+        return comicResponses.map((c) => c.json());
+      })
+      .then((ComicList) => {
+        ComicList.map((c) =>
+          c.then((data) => {
+            console.log(data);
+          })
+        );
+      });*/
+    //console.log(character);
+    return character;
+  }
+
+  document.addEventListener("DOMContentLoaded", async () => {
+    /* getCharacters().then((characters) => {
+      console.log(characters);
+    });*/
+    const container = document.querySelector("#container");
+    allCharacters(container, off, lim);
   });
 })();
---
+++ sudoku/exercici 3/sudoku.js
@@ -0,0 +1,243 @@
+(() => {
+  // prettier-ignore
+  let numeros = [
+    2, 9, 5, 6, 7, 8, 1, 4, 3,
+    6, 4, 3, 9, 5, 1, 8, 7, 2,
+    8, 7, 1, 3, 4, 2, 5, 9, 6,
+    7, 1, 2, 5, 6, 9, 3, 8, 4,
+    3, 6, 8, 7, 1, 4, 9, 2, 5,
+    4, 5, 9, 8, 2, 3, 6, 1, 7,
+    9, 2, 7, 1, 3, 6, 4, 5, 0, //falten els 8
+    5, 8, 6, 4, 9, 7, 2, 3, 0, // falta el 1
+    1, 3, 4, 2, 0, 5, 7, 6, 9,
+];
+  let item = 0;
+
+  function descargarSudokus() {
+    fetch("coleccionsudokus.json")
+      .then((s) => s.json())
+      .then((s) => {
+        let keys = Object.keys(s.sudokus);
+        let existingKeys = [];
+        if (localStorage.getItem("solucionades")) {
+          existingKeys = localStorage.getItem("solucionades").split(" ");
+        }
+        keys = keys.filter((k) => existingKeys.indexOf(k) == -1);
+        item = keys[Math.floor(Math.random() * keys.length)];
+        numeros = s.sudokus[item];
+
+        // console.log( Object.keys(s.sudokus), keys ,s.sudokus[item]);
+      })
+      .then(generar);
+  }
+
+  function generar() {
+    let divSudoku = document.getElementById("sudoku");
+    let tablaSudoku = document.createElement("table");
+    for (let i = 0; i < 9; i++) {
+      // files
+      let fila = document.createElement("tr");
+      if ((i == 2) | (i == 5)) fila.className = "separador";
+      tablaSudoku.append(fila);
+      for (let j = 0; j < 9; j++) {
+        // columnes
+        let numero = numeros[i * 9 + j];
+        let celda = document.createElement("td");
+        celda.id = `celda-${i}-${j}`;
+        if ((j == 2) | (j == 5)) {
+          celda.className = "separador";
+        }
+        celda.innerHTML = `<span>${numero > 0 ? numero : ""}</span>`;
+        if (numero == 0) {
+          // Es pot editar
+          let span = celda.querySelector("span");
+          span.classList.add("editable");
+          // span.contentEditable = true;
+          // El llevem d'ací per tindre un millor control
+          /* span.addEventListener("keydown", function(event) {
+                    if (this.innerText.length === 1 && event.key != "Backspace") {
+                        event.preventDefault();
+                    }
+                });*/
+        }
+        fila.append(celda);
+      }
+    }
+    divSudoku.append(tablaSudoku);
+  }
+
+  let valid = true;
+
+  function validar(actual) {
+    // Versió molt imperativa
+    //console.log(actual.parentElement);
+
+    let posicionActual = actual.parentElement.id
+      .split("-")
+      .filter((e) => e != "celda")
+      .map((e) => parseInt(e));
+
+    // console.log(posicionActual);
+
+    let valid = true;
+    let numeros = [];
+
+    document.querySelectorAll("td").forEach((e) => (e.className = "")); //reset
+
+    let files = document.querySelectorAll("tr");
+    for (let i = 0; i < files.length; i++) {
+      let columnes = files[i].querySelectorAll("span");
+      for (let j = 0; j < columnes.length; j++) {
+        numeros.push(parseInt(columnes[j].innerText));
+      }
+    }
+    //    console.log(numeros);
+    // Comprovar files
+    for (let i = 0; i < 9; i++) {
+      // cada fila
+      let fila = new Set();
+      for (let j = 0; j < 9; j++) {
+        const element = numeros[i * 9 + j];
+        !isNaN(element) && fila.add(element);
+      }
+      let valida = fila.size == 9;
+
+      if (!valida) {
+        console.warn("Fila no vàlida", { fila });
+        valid = false;
+        if (i == posicionActual[0]) {
+          // si es la fila correspondiente
+          actual.parentElement.parentElement
+            .querySelectorAll("td")
+            .forEach((e) => e.classList.add("mal"));
+        }
+      }
+    }
+
+    // Comprovar columnes
+    for (let i = 0; i < 9; i++) {
+      // cada columna
+      let columna = new Set();
+      for (let j = 0; j < 9; j++) {
+        const element = numeros[j * 9 + i];
+        !isNaN(element) && columna.add(element);
+      }
+      let valida = columna.size == 9;
+
+      if (!valida) {
+        console.warn("columna no vàlida", { columna });
+        valid = false;
+        if (i == posicionActual[1]) {
+          // si es la fila correspondiente
+          let expresion = new RegExp(`celda-[0-9]-${i}`);
+          let mismaColumna = [...document.querySelectorAll("td")].filter((e) =>
+            expresion.test(e.id)
+          );
+          //console.log(mismaColumna);
+          mismaColumna.forEach((e) => e.classList.add("mal"));
+        }
+      }
+    }
+
+    // Comprovar quadrats
+    for (let i = 0; i < 3; i++) {
+      // cada fila de quadrats
+      for (let j = 0; j < 3; j++) {
+        //cada columna de quadrats
+        let quadrat = new Set();
+        for (let l = 0; l < 3; l++) {
+          //dins del quadrat
+          for (let m = 0; m < 3; m++) {
+            const element = numeros[i * 27 + j * 3 + l * 9 + m];
+            // i és el desplaçament de files de quadrats 3*9
+            // j és el desplaçament en eixa fila per columnes que són de 3
+            // l és el desplaçament en files dins de la fila de quadrats, files de 9
+            // m és el desplaçament dins del quadrat
+            !isNaN(element) && quadrat.add(element);
+          }
+        }
+        let valida = quadrat.size == 9;
+
+        if (!valida) {
+          console.warn("quadrat no vàlid", { quadrat });
+          valid = false;
+          if (
+            i == Math.floor(posicionActual[0] / 3) &&
+            j == Math.floor(posicionActual[1] / 3)
+          ) {
+            // si es la fila correspondiente
+            //console.log({posicionActual});
+            let ii = i * 3,
+              jj = j * 3;
+            let mismoCuadrado = [
+              [ii, jj],
+              [ii + 1, jj],
+              [ii + 2, jj],
+              [ii, jj + 1],
+              [ii + 1, jj + 1],
+              [ii + 2, jj + 1],
+              [ii, jj + 2],
+              [ii + 1, jj + 2],
+              [ii + 2, jj + 2],
+            ];
+            mismoCuadrado = mismoCuadrado.map((e) =>
+              document.querySelector(`#celda-${e[0]}-${e[1]}`)
+            );
+            // console.log({mismoCuadrado});
+            mismoCuadrado.forEach((e) => e.classList.add("mal"));
+          }
+        }
+      }
+    }
+
+    if (valid == false) {
+      document.querySelector("#sudoku table").classList.add("mal");
+    } else {
+      // solucionat
+      document.querySelector("#sudoku table").classList.add("bien");
+      if (localStorage.getItem("solucionades")) {
+        let solucionades = localStorage.getItem("solucionades");
+        solucionades = `${solucionades} ${item}`;
+        localStorage.setItem("solucionades", solucionades);
+      } else {
+        localStorage.setItem("solucionades", `${item}`);
+      }
+    }
+  }
+
+  document.addEventListener("DOMContentLoaded", function () {
+    descargarSudokus();
+
+    //  document.querySelector('#generar').addEventListener('click',generar);
+
+    document.querySelector("#sudoku").addEventListener("click", (event) => {
+      //console.log(event.target);
+      let celda = event.target;
+      if (celda.tagName == "TD") {
+        celda = celda.querySelector("span");
+      }
+      // console.log({celda});
+
+      if (celda.classList.contains("editable")) {
+        let teclado = document.createElement("div");
+        teclado.innerHTML = `<span>1</span>
+        <span>2</span>
+        <span>3</span>
+        <span>4</span>
+        <span>5</span>
+        <span>6</span>
+        <span>7</span>
+        <span>8</span>
+        <span>9</span>
+        `;
+        teclado.classList.add("teclado");
+        celda.parentElement.append(teclado);
+        teclado.addEventListener("click", (e) => {
+          celda.innerText = e.target.innerText;
+          teclado.remove();
+          validar(celda);
+        });
+      }
+    });
+  });
+})();
