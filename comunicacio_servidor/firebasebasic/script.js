8b87a8c33779bfcbe226a0990ba536b82160c76e
xxjcaxx <jocasal@gmail.com>
2021-11-10 11:18:53 +0100

    dimecres 10 firebase

--- comunicacio_servidor/firebasebasic/script.js
+++ comunicacio_servidor/firebasebasic/script.js
@@ -11,10 +11,16 @@ async function firebaseProductes() {
         datos[key].id = key;
         productes.push(datos[key]);
       }
-      console.log(productes);
+      let divProductes = document.querySelector("#productes");
+      divProductes.innerHTML = "";
+      productes.map((p) => {
+        let divProducte = document.createElement("div");
+        divProducte.innerHTML = `<h2>${p.nom}</h2><img src="${p.img}"/><p>${p.preu}</p>`;
+        divProductes.append(divProducte);
+      });
     });
 
-  let item = { nom: "android", preu: "200" };
+  //let item = { nom: "android", preu: "200" };
 
   /*fetch(url, {
     method: "post",
@@ -28,7 +34,7 @@ async function firebaseProductes() {
     });
   */
 
-  let prod = productes[0];
+  /* let prod = productes[0];
   prod.preu = 123456;
   let key = prod.id;
   delete prod.id;
@@ -42,7 +48,40 @@ async function firebaseProductes() {
     .then((datos) => {
       // tractament de les dades
       console.log(datos);
-    });
+    });*/
+}
+function encodeImageFileAsURL(element) {
+  var file = element.files[0];
+  var reader = new FileReader();
+  reader.onloadend = function () {
+    console.log("RESULT", reader.result);
+    element.imagen = reader.result;
+  };
+  reader.readAsDataURL(file);
 }
 
-firebaseProductes();
+document.addEventListener("DOMContentLoaded", () => {
+  firebaseProductes();
+  document.querySelector("#img").addEventListener("change", function () {
+    encodeImageFileAsURL(this);
+  });
+  document.querySelector("#crear").addEventListener("click", function (event) {
+    event.preventDefault();
+    let producteNou = {
+      nom: document.querySelector("#nom").value,
+      preu: document.querySelector("#preu").value,
+      img: document.querySelector("#img").imagen,
+    };
+    console.log(producteNou);
+
+    fetch(url + ".json", {
+      method: "post",
+      headers: { "Content-type": "application/json; charset=UTF-8" },
+      body: JSON.stringify(producteNou),
+    })
+      .then((response) => response.json())
+      .then((datos) => {
+        firebaseProductes();
+      });
+  });
+});
