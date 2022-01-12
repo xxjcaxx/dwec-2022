ce5ff098c5e4382e526c340228d0fcf99da2cad4
xxjcaxx <xxjcaxx@yahoo.es>
2022-01-12 17:42:57 +0100

    mapa

---
+++ Angular/mapa/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { datos } from './datos';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'mapa';
+  datos = datos;
+}
