98d8c0316715a3863e0aa559b0beb937a4f2e8e4
xxjcaxx <jocasal@gmail.com>
2022-01-14 09:44:32 +0100

    mapa3

--- Angular/mapa/src/app/app.component.ts
+++ Angular/mapa/src/app/app.component.ts
@@ -9,4 +9,19 @@ import { datos } from './datos';
 export class AppComponent {
   title = 'mapa';
   datos = datos;
+  datos_original = {...datos};
+  filtre='';
+
+  filtrar = ()=>{
+    this.datos.features = this.datos_original.features.filter(d=> d.properties.descripcio.includes(this.filtre))
+  }
+
+  ordenar = (criteri:string)=>{
+    if(criteri=='nom'){
+      this.datos.features = this.datos.features.sort((a,b)=>{return a.properties.descripcio > b.properties.descripcio ? 1: -1})
+    }
+    if(criteri=='id'){
+      this.datos.features = this.datos.features.sort((a,b)=>{return a.properties.id > b.properties.id ? 1: -1})
+    }
+  }
 }
--- Angular/my-app/src/app/app.module.ts
+++ Angular/my-app/src/app/app.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
