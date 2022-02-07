349df23cff368435c1f483191ccdf56920a108aa
xxjcaxx <jocasal@gmail.com>
2022-02-07 10:33:59 +0100

    graph

--- Angular/negocity/src/app/components/points/points.component.ts
+++ Angular/negocity/src/app/components/points/points.component.ts
@@ -13,7 +13,26 @@ export class PointsComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+
+    this.graphData = [
+      {
+        name: 's1', 
+        series: [
+          {name: '1', value: 20},
+          {name: '2', value: 10},
+          {name: '3', value: 30},
+          {name: '4', value: 20},
+          {name: '5', value: 25},
+          {name: '6', value: 21},
+          {name: '7', value: 0},
+          {name: '8', value: 2}
+        ]
+      }
+    ]
+
   }
+
+
 
   view: [number,number] = [700, 700];
 
