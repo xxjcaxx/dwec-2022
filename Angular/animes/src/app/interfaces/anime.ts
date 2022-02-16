d0154ab88a1efbc6ec75ae600685ef7def081df7
xxjcaxx <xxjcaxx@yahoo.es>
2022-02-16 16:46:25 +0100

    animes

---
+++ Angular/animes/src/app/interfaces/anime.ts
@@ -0,0 +1,16 @@
+export interface Anime {
+animeSeason: {
+  season: string,
+  year: number,
+},
+episodes: number,
+picture: string,
+relations: string[],
+sources: string[],
+status: string,
+tags: string[]
+thumbnail: string,
+title: string,
+type: string
+
+}
