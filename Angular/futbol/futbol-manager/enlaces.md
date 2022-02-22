31edcb0d35a5c2af6fe888bf8ce290867659affa
xxjcaxx <jocasal@gmail.com>
2022-02-22 17:42:04 +0100

    futbol

---
+++ Angular/futbol/futbol-manager/enlaces.md
@@ -0,0 +1,3 @@
+https://tailwindcss.com/docs/guides/angular
+https://marketplace.visualstudio.com/items?itemName=bradlc.vscode-tailwindcss
+https://flowbite.com/docs/components/navbar/
--- Angular/futbol/futbolsdk/scripts.js
+++ Angular/futbol/futbolsdk/scripts.js
@@ -1,20 +1,18 @@
-const app = new Realm.App({ id: 'futbol-rqxxa' });
+const app = new Realm.App({ id: "futbol-rqxxa" });
 
-
-
-
-async function loginAnonymous() { // https://docs.mongodb.com/realm/web/authenticate/#std-label-web-login-anonymous
-    // Create an anonymous credential
-    const credentials = Realm.Credentials.anonymous();
-    try {
-      // Authenticate the user
-      const user = await app.logIn(credentials);
-      // `App.currentUser` updates to match the logged in user
-      console.assert(user.id === app.currentUser.id)
-      return user
-    } catch(err) {
-      console.error("Failed to log in", err);
-    }
+async function loginAnonymous() {
+  // https://docs.mongodb.com/realm/web/authenticate/#std-label-web-login-anonymous
+  // Create an anonymous credential
+  const credentials = Realm.Credentials.anonymous();
+  try {
+    // Authenticate the user
+    const user = await app.logIn(credentials);
+    // `App.currentUser` updates to match the logged in user
+    console.assert(user.id === app.currentUser.id);
+    return user;
+  } catch (err) {
+    console.error("Failed to log in", err);
+  }
 }
 /*  loginAnonymous().then(user => {
     console.log("Successfully logged in!", user);
@@ -26,71 +24,150 @@ async function loginAnonymous() { // https://docs.mongodb.com/realm/web/authenti
 
   })*/
 
+async function insertar1Equipo() {
+  let equipos = await fetch("equipos.json");
+  equipos = await equipos.json();
+  console.log(equipos);
+  const mongodb = app.currentUser.mongoClient("mongodb-atlas");
+  const equips = mongodb.db("futbol").collection("equips");
+  //https://docs.mongodb.com/realm/web/mongodb/#insert-a-single-document
+  const result = await equips.insertOne(equipos[2]);
+  console.log(result);
+}
 
-  async function insertar1Equipo(){
-    let  equipos = await fetch('equipos.json');
-    equipos = await equipos.json();
-    console.log(equipos); 
-    const mongodb = app.currentUser.mongoClient('mongodb-atlas');
-    const equips = mongodb.db("futbol").collection("equips");
-//https://docs.mongodb.com/realm/web/mongodb/#insert-a-single-document
-    const result = await equips.insertOne(equipos[2]);
-    console.log(result);
-  }
-
-
-  async function insertarEquipos(){
-    let  equipos = await fetch('equipos.json');
-    equipos = await equipos.json();
-    console.log(equipos); 
-    const mongodb = app.currentUser.mongoClient('mongodb-atlas');
-    const equips = mongodb.db("futbol").collection("equips");
-//https://docs.mongodb.com/realm/web/mongodb/#insert-a-single-document
-    let count = 0;
-    let max_count = 100;
-    let bufferEquipos = [];
-    for (let e of equipos){
-        if(e){
-            count ++;
-            bufferEquipos.push(e);
-            if (count >= max_count){
-                 const result = await equips.insertMany(bufferEquipos);
-                 console.log('Insertados: ',result);
-                 count = 0; bufferEquipos = [];
-            }
-        }
+async function insertarEquipos() {
+  let equipos = await fetch("equipos.json");
+  equipos = await equipos.json();
+  console.log(equipos);
+  const mongodb = app.currentUser.mongoClient("mongodb-atlas");
+  const equips = mongodb.db("futbol").collection("equips");
+  //https://docs.mongodb.com/realm/web/mongodb/#insert-a-single-document
+  let count = 0;
+  let max_count = 100;
+  let bufferEquipos = [];
+  for (let e of equipos) {
+    if (e) {
+      count++;
+      bufferEquipos.push(e);
+      if (count >= max_count) {
+        const result = await equips.insertMany(bufferEquipos);
+        console.log("Insertados: ", result);
+        count = 0;
+        bufferEquipos = [];
+      }
     }
-    const result = await equips.insertMany(bufferEquipos);
-    console.log("Ultimos",result);
-    //const result = await equips.insertMany(equipos[2]);
-    //console.log(result);
   }
+  const result = await equips.insertMany(bufferEquipos);
+  console.log("Ultimos", result);
+  //const result = await equips.insertMany(equipos[2]);
+  //console.log(result);
+}
 
+async function borrarEquipos() {
+  const mongodb = app.currentUser.mongoClient("mongodb-atlas");
+  const equips = mongodb.db("futbol").collection("equips");
+  //https://docs.mongodb.com/realm/web/mongodb/#insert-a-single-document
 
-  async function borrarEquipos(){
- 
-    const mongodb = app.currentUser.mongoClient('mongodb-atlas');
-    const equips = mongodb.db("futbol").collection("equips");
-//https://docs.mongodb.com/realm/web/mongodb/#insert-a-single-document
+  const result = await equips.deleteMany({});
+  console.log("Borrados", result);
+  //const result = await equips.insertMany(equipos[2]);
+  //console.log(result);
+}
 
-    const result = await equips.deleteMany({});
-    console.log("Borrados",result);
-    //const result = await equips.insertMany(equipos[2]);
-    //console.log(result);
-  }
-
-
-  document.addEventListener('DOMContentLoaded',()=>{
-    document.querySelector('#loginbtn').addEventListener('click',()=>{
-      loginAnonymous().then(user => {
-        console.log("Successfully logged in!", user);    
-      })
+document.addEventListener("DOMContentLoaded", () => {
+  document.querySelector("#loginbtn").addEventListener("click", () => {
+    loginAnonymous().then((user) => {
+      console.log("Successfully logged in!", user);
     });
+  });
 
-    document.querySelector('#insertarbtn').addEventListener('click',()=>{
-        insertarEquipos();
-    });
-    document.querySelector('#borrarbtn').addEventListener('click',()=>{
-        borrarEquipos();
-    });
+  document.querySelector("#insertarbtn").addEventListener("click", () => {
+    insertarEquipos();
+  });
+  document.querySelector("#borrarbtn").addEventListener("click", () => {
+    borrarEquipos();
+  });
+
+  document.querySelector("#mostrarScrap").addEventListener("click", () => {
+    mostrarScrap();
+  });
+
+  document.querySelector("#mostrarPlayers").addEventListener("click", () => {
+    mostrarPlayers();
+  });
 });
+
+async function mostrarScrap() {
+  let players = await fetch("scrap/marca-fantasy-scraper/laliga/todos.json");
+  players = await players.json();
+  console.log(players);
+
+  let onlyplayers = players.map((p) => {
+    p = { ...p };
+    delete p.team;
+    return p;
+  });
+
+  console.log(onlyplayers);
+
+  let onylteams = [...new Set(players.map((p) => p.team.id))]
+    .map((t) => players.find((p) => p.team.id == t))
+    .map((p) => p.team);
+
+  console.log(onylteams);
+
+  const mongodb = app.currentUser.mongoClient("mongodb-atlas");
+  const pdb = mongodb.db("futbol").collection("players");
+  // const result = await pdb.insertMany(onlyplayers);
+  // console.log("Players insert", result);
+
+  const tdb = mongodb.db("futbol").collection("liga");
+  //const resultt = await tdb.insertMany(onylteams);
+  //console.log("teams insert", resultt);
+}
+
+async function mostrarPlayers() {
+  let players = await fetch("scrap/marca-fantasy-scraper/laliga/todos.json");
+  players = await players.json();
+  console.log(players);
+
+  let onlyplayers = players.map((p) => {
+    p = { ...p };
+    delete p.team;
+    return p;
+  });
+
+  let divPlayers = document.querySelector("#players");
+
+  let imgs = await fetch("scrap/marca-fantasy-scraper/img/images.json");
+  imgs = await imgs.json();
+
+  onlyplayers.forEach((p) => {
+    // console.log(imgs);
+    //imgs.find((i) => true);
+    let currentImg = imgs.find((i) =>
+      p.images.transparent["256x256"].includes(i.name)
+    ); //.player = p.id;
+    //console.log(currentImg);
+    currentImg.player = p.id;
+  });
+  console.log(imgs);
+
+  //onlyplayers.forEach((p) => {
+  //let imgP = document.createElement("img");
+  // imgP.src = p.images.transparent["256x256"];
+  //divPlayers.append(imgP);
+
+  // });
+
+  const mongodb = app.currentUser.mongoClient("mongodb-atlas");
+  const pdb = mongodb.db("futbol").collection("images");
+  for (let i of imgs) {
+    const result = await pdb.insertOne(i);
+    console.log("Images insert", result);
+  }
+
+  divPlayers.innerHTML = JSON.stringify(
+    onlyplayers.map((p) => p.images.transparent["256x256"])
+  );
+}
