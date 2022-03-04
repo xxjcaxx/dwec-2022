759d5c7f7ff0ac3addad829d0f17d8c5474d643d
xxjcaxx <jocasal@gmail.com>
2022-03-04 09:23:45 +0100

    dis

--- tresenraya/inicial/tres.js
+++ tresenraya/inicial/tres.js
@@ -1,85 +1,111 @@
 "use strict";
 
-
 /////////// Funciones de relleno del tablero y eventos
 // Para trabajar con 0,1,2, debemos luego traducir lo que se muestra
-function translateCell(value){
-  const translations = ["",'◯','×'];
+function translateCell(value) {
+  const translations = ["", "◯", "×"];
   return translations[value];
 }
-function fillCell(value,cell){
- cell.innerHTML = translateCell(value);
+function fillCell(value, cell) {
+  cell.innerHTML = translateCell(value);
 }
 function fillBoard(board, data) {
-  Object.entries(data).forEach(position => fillCell(position[1], board.querySelector(`#${position[0]}`)));
-  
+  Object.entries(data).forEach((position) =>
+    fillCell(position[1], board.querySelector(`#${position[0]}`))
+  );
 }
-function addEvents(board,state){
-  function clickBoard(event){
-    clickCell(event.target,state);
-    fillBoard(board,state.getGame());
+function addEvents(board, state) {
+  function clickBoard(event) {
+    clickCell(event.target, state);
+    fillBoard(board, state.getGame());
   }
 
-  board.addEventListener('click',clickBoard);
+  board.addEventListener("click", clickBoard);
 }
-function clickCell(cell,state){
+function clickCell(cell, state) {
   const turn = state.getTurn();
   const game = state.getGame();
   game[cell.id] = turn;
   state.setGame(game);
-  state.getWinner();
+  const winner = state.getWinner();
+  if (winner != 0) {
+    console.log({ winner });
+  }
   state.switchTurn();
 }
 
-
-//////////// Estado del juego 
+//////////// Estado del juego
 /// Se pone en una función para mantener las closures y controlar su mutación
-function gameState(){  // Estado del juego en turnos
-  let turn = 1;  // closure para proteger el turno
-  // ignore-prettier
-  let game = {
-    pos1: 0, pos2: 0, pos3: 0,
-    pos4: 0, pos5: 0, pos6: 0,
-    pos7: 0, pos8: 0, pos9: 0,
-  }
+function gameState() {
+  // Estado del juego en turnos
+  let turn = 1; // closure para proteger el turno
+  // prettier-ignore
+  let game = { pos1: 0, pos2: 0, pos3: 0, pos4: 0, pos5: 0, pos6: 0, pos7: 0, pos8: 0, pos9: 0,};
+  console.log("game", game);
   return {
-    getTurn() { return turn;},
-    setTurn(t) {turn = t;},
-    switchTurn() { console.log(turn); turn == 1 ? turn = 2 : turn = 1; },
-    getGame() { return {...game}; },
-    setGame(g) { game = g; },
-    getWinner() { 
-      const winCombos = [[0,1,2],[3,4,5],[6,7,8],[0,4,8],[6,4,2],[0,3,6],[1,4,7],[2,5,8]];
-      function getPos(combo){
-        return [ Object.values(game)[combo[0]], Object.values(game)[combo[1]], Object.values(game)[combo[2]] ];
+    getTurn() {
+      return turn;
+    },
+    setTurn(t) {
+      turn = t;
+    },
+    switchTurn() {
+      console.log(turn);
+      turn == 1 ? (turn = 2) : (turn = 1);
+    },
+    getGame() {
+      return { ...game };
+    },
+    setGame(g) {
+      game = g;
+    },
+    getWinner() {
+      // prettier-ignore
+      const winCombos = [[0, 1, 2],[3, 4, 5],[6, 7, 8],[0, 4, 8],[6, 4, 2],[0, 3, 6],[1, 4, 7],[2, 5, 8]];
+      function getPos(combo) {
+        // prettier-ignore
+        return [Object.values(game)[combo[0]], Object.values(game)[combo[1]], Object.values(game)[combo[2]],
+        ];
       }
-      
-      if (winCombos.some(combo => getPos(combo).every( v=> v===1) ))  // Algunos combos dan todos iguales a 1
-      {
-        console.log('1 winner');
+      let winner = 0;
+      if (winCombos.some((combo) => getPos(combo).every((v) => v === 1))) {
+        winner = 1;
       }
-      if (winCombos.some(combo => getPos(combo).every( v=> v===2) ))
-      {
-        console.log('2 winner');
+      if (winCombos.some((combo) => getPos(combo).every((v) => v === 2))) {
+        winner = 2;
       }
-      
-    }
+      return winner;
+    },
+    reset() {
+      game = {
+        pos1: 0,
+        pos2: 0,
+        pos3: 0,
+        pos4: 0,
+        pos5: 0,
+        pos6: 0,
+        pos7: 0,
+        pos8: 0,
+        pos9: 0,
+      };
+    },
   };
 }
 
-function reset(){
+function reset(state) {
   const board = document.querySelector("#boardTable");
-  const state = gameState();
+  state.reset();
   fillBoard(board, state.getGame());
   //addEvents(board,state);
   return state;
 }
 
-
 //////// Inicio del juego
 document.addEventListener("DOMContentLoaded", function initialLoad() {
   // Se hace con declaración de función para rastrear mejor
-  //const state = reset();
-  addEvents( document.querySelector("#boardTable"),reset());
-  document.querySelector('#reset').addEventListener('click',reset);
+  const state = gameState();
+  addEvents(document.querySelector("#boardTable"), reset(state));
+  document
+    .querySelector("#reset")
+    .addEventListener("click", () => reset(state));
 });
