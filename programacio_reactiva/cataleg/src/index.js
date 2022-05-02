2365b9dd313e3dbc77eae2f61b8f550bd499ef13
xxjcaxx <jocasal@gmail.com>
2022-05-02 11:25:40 +0200

    cataleg

--- programacio_reactiva/cataleg/src/index.js
+++ programacio_reactiva/cataleg/src/index.js
@@ -1,15 +1,32 @@
-import { fromEvent, takeUntil } from "rxjs";
+import { debounceTime, fromEvent, of, takeUntil } from "rxjs";
 import "./style.css";
 
 document.addEventListener("DOMContentLoaded", () => {
   const divExemple1 = document.querySelector("#exemple1");
   fromEvent(divExemple1, "scroll")
     .pipe
-    // we will discuss cleanup strategies like this in future article
-    // takeUntil(userLeavesArticle)
-    ()
+    (
+      debounceTime(10)
+    )
     .subscribe((event) => {
-      // calculate and update DOM
-      console.log(event);
+      document.querySelector('#exemple1info').innerHTML = event.target.scrollTop;
     });
+
+    const source = of(1, 2, 3, 4, 5);
+    const subscribe = source.subscribe(val => 
+      document.querySelector('#exemple2info').innerHTML += val);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 });
